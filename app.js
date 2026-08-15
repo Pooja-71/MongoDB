7ba6908f7ea8070e0354605e7e1f7ce8fcd1eb51
Pooja-71 <poojakumari8546246@gmail.com>
2026-08-15 23:16:38 +0530

    practical

--- app.js
+++ app.js
@@ -1,35 +1,46 @@
 const express = require ('express');
 const app= express();
-const userModel = require('./usermodel')
+// const userModel = require('./usermodel')
+const path= require ("path");
+
+app.set("view engine","ejs");
+app.use(express.json());
+app.use(express.urlencoded({express:true}));
+app.use(express.static(path.join(__dirname, 'public')));
+
 
 app.get('/', (req, res) => {
-    res.send("hey");
+    res.render("index");
 })
 
-app.get("/create", async (req, res) => {
-  let createuser = await userModel.create({
-    name:"harsh",
-    username:"harsh",
-    email: "harsh@gmail.com"
-  })
-
-  res.send(createuser);
+app.get("/read", (req, res) => {
+  res.render("read");
 });
 
-app.get("/update", async (req, res) => {
+// app.get("/create", async (req, res) => {
+//   let createuser = await userModel.create({
+//     name:"harsh",
+//     username:"harsh",
+//     email: "harsh@gmail.com"
+//   })
+
+//   res.send(createuser);
+// });
+
+// app.get("/update", async (req, res) => {
   
-  let updateduser = await userModel.findOneAndUpdate({username: "harsh"}, {username:"harsh sharma"}, {new:true} )
-  res.send(updateduser);
-});
+//   let updateduser = await userModel.findOneAndUpdate({username: "harsh"}, {username:"harsh sharma"}, {new:true} )
+//   res.send(updateduser);
+// });
 
-app.get("/read", async (req, res) => {
-  let users = await userModel.find();
-  res.send(users);
-});
+// app.get("/read", async (req, res) => {
+//   let users = await userModel.find();
+//   res.send(users);
+// });
 
-app.get("/delete", async (req, res) => {
-  let users = await userModel.findOneAndDelete({username:"harsh"});
-  res.send(users);
-});
+// app.get("/delete", async (req, res) => {
+//   let users = await userModel.findOneAndDelete({username:"harsh"});
+//   res.send(users);
+// });
 
 app.listen(3000);
