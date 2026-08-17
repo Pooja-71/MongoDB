981881ba9e193ba61147d1da0c9c86d119528db5
Pooja-71 <poojakumari8546246@gmail.com>
2026-08-17 22:59:08 +0530

    encryption

--- app.js
+++ app.js
@@ -1,19 +1,24 @@
 const express = require("express");
 const cookieParser = require("cookie-parser");
+const bcrypt = require("bcrypt");
+
 
 const app = express();
 
 app.use(cookieParser());
 
 app.get("/", (req, res) => {
-  res.cookie("name", "harsh");
-  res.send("done");
+  bcrypt.genSalt(10, function (err, salt) {
+    bcrypt.hash("hehhehhe", salt, function (err, hash) {
+      console.log(hash);
+    });
+  });
 });
 
-app.get("/read", (req, res) => {
-  console.log(req.cookies);
-  res.send("read page");
-});
+// app.get("/read", (req, res) => {
+//   console.log(req.cookies);
+//   res.send("read page");
+// });
 
 // const path = require("path");
 // const userModel = require("./models/user");
