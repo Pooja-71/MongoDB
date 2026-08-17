476c717f54b72e55e4b94516128629e15f8f8834
Pooja-71 <poojakumari8546246@gmail.com>
2026-08-17 22:44:45 +0530

    cookie

--- app.js
+++ app.js
@@ -1,59 +1,74 @@
 const express = require("express");
+const cookieParser = require("cookie-parser");
+
 const app = express();
-const path = require("path");
-const userModel = require("./models/user");
 
-app.set("view engine", "ejs");
+app.use(cookieParser());
 
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-
-app.use(express.static(path.join(__dirname, "public")));
-
-// Home page
 app.get("/", (req, res) => {
-  res.render("index");
+  res.cookie("name", "harsh");
+  res.send("done");
 });
 
-// Read all users
-app.get("/read", async (req, res) => {
-  let users = await userModel.find();
-  res.render("read", { users });
+app.get("/read", (req, res) => {
+  console.log(req.cookies);
+  res.send("read page");
 });
 
-// Edit user
-app.get("/edit/:id", async (req, res) => {
-  let user = await userModel.findOne({ _id: req.params.id });
-  res.render("edit", { user });
-});
+// const path = require("path");
+// const userModel = require("./models/user");
 
-// Update user
-app.post("/update/:id", async (req, res) => {
-  let { name, email, imageurl } = req.body;
+// app.set("view engine", "ejs");
 
-  let user = await userModel.findOneAndUpdate(
-    { _id: req.params.id },
-    {
-      name,
-      email,
-      imageurl
-    },{new:true}
-  );
-  res.redirect("/read");
-});
+// app.use(express.json());
+// app.use(express.urlencoded({ extended: true }));
 
-// Create user
-app.post("/create", async (req, res) => {
-  let { name, email, imageurl } = req.body;
+// app.use(express.static(path.join(__dirname, "public")));
 
-  let createdUser = await userModel.create({
-    name,
-    email,
-    image: imageurl,
-  });
+// // Home page
+// app.get("/", (req, res) => {
+//   res.render("index");
+// });
 
-  res.redirect("/read");
-});
+// // Read all users
+// app.get("/read", async (req, res) => {
+//   let users = await userModel.find();
+//   res.render("read", { users });
+// });
+
+// // Edit user
+// app.get("/edit/:id", async (req, res) => {
+//   let user = await userModel.findOne({ _id: req.params.id });
+//   res.render("edit", { user });
+// });
+
+// // Update user
+// app.post("/update/:id", async (req, res) => {
+//   let { name, email, imageurl } = req.body;
+
+//   let user = await userModel.findOneAndUpdate(
+//     { _id: req.params.id },
+//     {
+//       name,
+//       email,
+//       imageurl
+//     },{new:true}
+//   );
+//   res.redirect("/read");
+// });
+
+// // Create user
+// app.post("/create", async (req, res) => {
+//   let { name, email, imageurl } = req.body;
+
+//   let createdUser = await userModel.create({
+//     name,
+//     email,
+//     image: imageurl,
+//   });
+
+//   res.redirect("/read");
+// });
 
 app.listen(3000, () => {
   console.log("Server is running on http://localhost:3000");
